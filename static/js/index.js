3d50e18f8827c09f42c7bd1ae32675da0821d224
Bouchra Mohamed Lemine <bouchramohamedcheikh@gmail.com>
2023-12-17 14:11:59 +0000

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,40 @@
+  window.addEventListener("load", function (){
+ 
+ /// showing the questions about history
+ var t1= document.querySelector(".topic1");
+    t1.addEventListener("click", function(){
+	 
+    window.location.href = '/history';
+ })
+ 
+ /// showing the questions about geography 
+ var t2= document.querySelector(".topic2");
+  t2.addEventListener("click", function(){
+	 
+     window.location.href = '/geography';
+ })
+ 
+ /// showing the questions about animals 
+ var t3= document.querySelector(".topic3");
+  t3.addEventListener("click", function(){
+	 
+     window.location.href = '/animals';
+ })
+ 
+ /// showing the questions about sports
+ var t4= document.querySelector(".topic4");
+  t4.addEventListener("click", function(){
+	 
+    window.location.href = '/sports';
+ }) 
+ 
+ /// showing the questions about maths
+ var t5= document.querySelector(".topic5");
+  t5.addEventListener("click", function(){
+	 
+    window.location.href = '/maths';
+ })
+ 
+  });
+  
+  
